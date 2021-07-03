a18d2a24c79b454101d972e0a5b4a2a1db99339b
Tecow Media <tecowmedia@gmail.com>
2021-07-03 11:37:36 -0400

    fixed path

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 // server/index.js
-
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../app/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -14,7 +14,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../app/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
