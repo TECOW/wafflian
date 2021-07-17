a8644e32cc150de18ef535eec45fa8e588a548ac
David <david4han@gmail.com>
2021-07-17 11:19:27 -0400

    First version of responsive text

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,25 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(()=> {
-    fetch("/api")
-    .then((res)=>res.json())
-    .then((data) => setData(data.message));
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
-      </header>
+    <div className="container">
+      <div className="row">
+
+        <div className="col-sm">
+          <h1 className="webTitle">Wafflian</h1>
+        </div>
+        <div className="col-sm">
+          <h2>Belgian Waffle & Cupcake Bar</h2>
+        </div>
+        <div className="col-sm">
+          <p>Monday - Saturday: 9:30 A.M. - 6:00 P.M.</p>
+        </div>
+
+      </div>
     </div>
+
+
   );
 }
 
