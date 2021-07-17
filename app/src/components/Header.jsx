76ef173ab120e676213be83943431c064a14532f
Tecow Media <tecowmedia@gmail.com>
2021-07-17 10:19:54 -0400

    WAF-14: fixed image path

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,12 +4,16 @@ import Navbar from "react-bootstrap/Navbar";
 
 
 function Header() {
+
+  const PUBLIC_URL = process.env.PUBLIC_URL || null;
+  console.log(PUBLIC_URL);
+
   return(
     <Navbar className="Header">
       <Navbar.Brand className="navbar-brand" href="#home">Home</Navbar.Brand>
       <Navbar.Brand className="navbar-brand" href="#menu">Menu</Navbar.Brand>
       <Navbar.Brand className="navbar-brand" href="#contact">Contact</Navbar.Brand>
-      <a href="#home" ><img className="logo" src=".../public/images/WafflianLogo.png" alt="logo"></img></a>
+      <a href="#home" ><img className="logo" src={PUBLIC_URL ? PUBLIC_URL + "/images/WafflianLogo.png" : "/images/WafflianLogo.png"} alt="logo"></img></a>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
     </Navbar>
   )
