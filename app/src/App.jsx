f5f7e095adc6cd19831477b99b03989d7088e27b
David <david4han@gmail.com>
2021-07-24 10:49:08 -0400

    Re-added font for title

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,25 +6,27 @@ import Header from "./components/Header.jsx";
 function App() {
 
   return (
-        <Container>
-          <Row className="content align-items-center">
-            <Col>
-              <Image src="/images/Cupcakes.png"/>
-            </Col>
-            <Col className="beginningInfo">
-              <Row>
-                <h1 className="webTitle">Wafflian</h1>
-              </Row>
-              <Row>
-                <h2>Belgian Waffle & Cupcake Bar</h2>
-              </Row>
-              <Row>
-                <p>Monday - Saturday: 9:30 A.M. - 6:00 P.M.</p>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-
+    <div className="App">
+      <Header/>
+      <Container>
+        <Row className="content align-items-center">
+          <Col>
+            <Image src="/images/Cupcakes.png"/>
+          </Col>
+          <Col className="beginningInfo">
+            <Row>
+              <h1 className="webTitle">Wafflian</h1>
+            </Row>
+            <Row>
+              <h2>Belgian Waffle & Cupcake Bar</h2>
+            </Row>
+            <Row>
+              <p>Monday - Saturday: 9:30 A.M. - 6:00 P.M.</p>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
 
   );
 }
