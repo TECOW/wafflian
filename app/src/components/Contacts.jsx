9d716b568001681efa07571245ebb64458d086cc
Isaac <isaaccha7@gmail.com>
2021-07-31 11:42:34 -0400

    debug contacts

--- app/src/components/Contacts.jsx
+++ app/src/components/Contacts.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import Header from './Header';
-import List from './List';
+import {Container, Row, Col, Image} from 'react-bootstrap';
 import '../styles/Contacts.css'
 
 function Contacts() {
   return (
-  <div>
-    <Header />
-    <List />
-  </div>
+    <Container>
+      <Row>
+        <Col>
+          <Row className="list">
+            <h1 className="header">
+                Our Contact Details
+            </h1>
+          </Row>
+          <Row className="list">
+            <Row>
+                <h4 className="headings">Mailing Address</h4>
+                <p className="text">109 McCaul St, 28B</p>
+            </Row>
+            <Row>
+              <h4 className="headings">Email Address</h4>
+              <p className="text">Wafflian@gmail.com</p>
+            </Row>
+            <Row>
+              <h4 className="headings">Phone Number</h4>
+              <p className="text">(647)-748-5788</p>
+              <p className="text">(416)-508-6337</p>
+            </Row>
+            <Row>
+              <h4 className="headings">Mon - Sat</h4>
+              <p className="text">9:30AM - 6:00PM</p>
+            </Row>
+          </Row>
+        </Col>
+          <Col className="picture">
+            <Image className="shop" src="/images/shop.jpg" rounded />
+          </Col>
+      </Row>
+    </Container>
   );
 }
-
 
 export default Contacts;
