d89ab59141c8f8d496feada8b821e072b150a69d
Enoch <kkachiboy@gmail.com>
2021-07-31 10:57:37 -0400

    changed the codes

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -9,21 +9,21 @@ function Footer() {
 
   return(
     <Navbar className="footer">
-    <Col/>
-    <Col>
-      <a href="https://www.instagram.com/toronto.wafflian/">
-      <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Instagram.png" : "/images/Instagram.png"} alt="logo" rounded/>
-      </a>
-      <a href="https://www.facebook.com/Wafflian/">
-      <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Facebook.png" : "/images/Facebook.png"} alt="logo" rounded/>
-      </a>
-      <a href="https://twitter.com/wafflian">
-      <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Twitter.png" : "/images/Twitter.png"} alt="logo" rounded/>
-      </a>
-    </Col>
-    <Col>
-      Copyright
-    </Col>
+      <Col/>
+      <Col>
+        <a href="https://www.instagram.com/toronto.wafflian/">
+          <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Instagram.png" : "/images/Instagram.png"} alt="logo" rounded/>
+        </a>
+        <a href="https://www.facebook.com/Wafflian/">
+          <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Facebook.png" : "/images/Facebook.png"} alt="logo" rounded/>
+        </a>
+        <a href="https://twitter.com/wafflian">
+          <Image className="social" src={PUBLIC_URL ? PUBLIC_URL + "/images/Twitter.png" : "/images/Twitter.png"} alt="logo" rounded/>
+        </a>
+      </Col>
+      <Col>
+        Copyright
+      </Col>
     </Navbar>
   )
 };
