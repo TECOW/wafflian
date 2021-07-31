b288e38dfe0f56ad0d3635a74af97ffdc470d657
Tecow Media <tecowmedia@gmail.com>
2021-07-31 11:58:43 -0400

    baked goods moved to server, menu now parsing from json

--- app/src/components/Menus.jsx
+++ app/src/components/Menus.jsx
@@ -1,45 +1,39 @@
+import React, {useEffect, useState} from 'react';
 import "../styles/Menus.css";
-import React from "react";
 import {Container, Row, Col, Image, Carousel} from "react-bootstrap";
 
 function Menus() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+    }, []);
+
   return (
-    <div className="menus">
-      <Container className="menu">
-        <Row>
-          <h1 className="center">Menu</h1>
-        </Row>
-        <Row>
-          <h2 className="center">Cupcakes</h2>
-        </Row>
-
-          <Carousel className="carouselMargins">
-            <Carousel.Item>
-              <Image className="d-block dimensions imageMargin" src="/images/VanillaCupcake.png" alt="VanillaCupcake" roundedCircle/>
-
-              <Carousel.Caption className="caption">
-                <Row>
-                  <h3 className="center">Vanilla Cupcake</h3>
-                </Row>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <Image className="d-block dimensions imageMargin" src="/images/MatchaCupcake.png" alt="MatchaCupcake" roundedCircle/>
-
-              <Carousel.Caption className="caption">
-                <Row>
-                  <h3 className="center">Matcha Cupcake</h3>
-                </Row>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-
-
-
-          </Carousel>
-      </Container>
-    </div>
+    <Container className="menu">
+      <Row>
+        <h1 className="center">Menu</h1>
+      </Row>
+      <Row>
+        <h2 className="center">Cupcakes</h2>
+      </Row>
+        <Carousel className="carouselMargins">
+          {products.map(product => {
+            return (
+              <Carousel.Item>
+                <Image className="d-block dimensions imageMargin" src={product.imagePath} alt={product.name} roundedCircle/>
+                <Carousel.Caption className="caption">
+                  <Row>
+                    <h3 className="center">{product.name}</h3>
+                  </Row>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+    </Container>
   )
 }
 export default Menus;
